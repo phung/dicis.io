7b6f8d33b787133a079c2129a3f99786ca8b2d93
Satyam Gupta <maytas92@yahoo.com>
2013-07-13 13:33:04 -0700

    Results page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130713121309) do
+ActiveRecord::Schema.define(version: 20130713201452) do
 
   create_table "events", force: true do |t|
     t.datetime "created_at"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20130713121309) do
     t.integer  "count"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "optionName"
   end
 
 end
