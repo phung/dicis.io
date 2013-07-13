a839f6974b0b4a0cb8ed10560d6ad8fa9a865be5
Satyam Gupta <sagupta@sagupta-mn2.linkedin.biz>
2013-07-12 23:26:01 -0700

    Initial Rails App

---
+++ dicis.io/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DicisIo::Application.config.secret_key_base = '4bca1990e11b4b22dd5bd376938397d24a7cc2dfbf3cad1d8854d6eb4b1d82dd7c953bb8e1dd6c46d738c7ad46f8723e3b7ea5636e38a1e2d7daece95af177a0'
