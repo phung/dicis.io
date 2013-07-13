4a530c584ad510f84060b5634701927ae7cda5b6
Satyam Gupta <maytas92@yahoo.com>
2013-07-13 01:46:57 -0700

    Added vote resource

---
+++ app/views/votes/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @vote, :count, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 DicisIo::Application.routes.draw do
+  resources :votes
+
   resources :events
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'events#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
